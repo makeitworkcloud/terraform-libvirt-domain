218dd6dcda1f38a4af4e7bf8de54820b30dc60ad
Steven Welch <steven@makeitwork.cloud>
2025-12-21 00:28:19 -0700

    feat: initial libvirt domain module for provider v0.9.x (#1)

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.3"
+
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = ">= 0.9.0"
+    }
+    aap = {
+      source = "registry.terraform.io/ansible/aap"
+    }
+  }
+}
